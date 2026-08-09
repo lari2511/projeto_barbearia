5555378ff5cb0d26002420c3dd622cc1fbd13776
larissa nascimento <lari.nascimento20148@gmail.com>
2026-08-09 10:12:01 -0300

    Build frontend and deploy updated CEP-only validation

---
+++ app/static/assets/index-CN7xXkN-.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CblDLutr.js","./index-Dkggivyr.js","./index-BnOUsn2a.css"])))=>i.map(i=>d[i]);
+import{r,_ as p}from"./index-Dkggivyr.js";const o=r("App",{web:()=>p(()=>import("./web-CblDLutr.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{o as App};
